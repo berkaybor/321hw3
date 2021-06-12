5975d8dd3140600d9c69cebd86cc51123f8f5306
Berkay Bör <berkay.bor07@gmail.com>
2021-06-12 22:44:20 +0300

    changes...

--- users/database.py
+++ users/database.py
@@ -1,10 +1,23 @@
 from django.db import connection
 
 
-def check_creditentials(name, surname, password):
-    if(name == "berkay" and institution == "boun" and password == 'qwer1234'):
-        return True
-    return False 
+def check_credentials(name, institution, password):
+    query = 'select * from users where username = "{}" and institution = "{}" and password = "{}"'.format(name, institution, password)
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+        tmp = cursor.fetchall()
+        if tmp:
+            return True
+        return False
+
+def check_credentials_db_manager(username, password):
+    query = 'select * from database_managers where managerusername = "{}" and password = "{}"'.format(username, password)
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+        tmp = cursor.fetchall()
+        if tmp:
+            return True
+        return False
 
 def return_users():
     stmt = 'SELECT * FROM users;'
@@ -14,5 +27,16 @@ def return_users():
     users = []
     for u in tmp:
         x = {"username":u[0], "institution":u[1], "password":u[2]}
-        users.append(x)    
+        users.append(x)
     return users
+
+
+def add_user_to_db(name, institution, password):
+    query = "INSERT INTO users VALUES ('{}', '{}', '{}')".format(name, institution, password)
+    with connection.cursor() as cursor:
+        try:
+            cursor.execute(query)
+        except:
+            return 'User creation failed: User exists or bad input'
+
+        return
--- users/views.py
+++ users/views.py
@@ -1,12 +1,72 @@
 from django.shortcuts import render
-from .database import check_creditentials, return_users
-
+from .database import check_credentials, return_users, check_credentials_db_manager, add_user_to_db
+import hashlib
+from .forms import UserLoginForm, DBManagerLoginForm, UserAddForm
 
 def login(request):
     if request.method == 'GET':
-        return render(request, 'users/login.html')
+
+        context = {'form': UserLoginForm()}
+        return render(request, 'users/login.html', context)
+
     elif request.method == 'POST':
-        users = return_users()
-        users_dict = {'user_list': users}
-        return render(request, 'users/login.html', users_dict)
+
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            f = form.cleaned_data
+            sha256password = hashlib.sha256(f['password'].encode('utf-8')).hexdigest()
+            if check_credentials(f['name'], f['institution'], sha256password):
+                
+                request.session['user_logged_in'] = True
+                request.session['database_manager_logged_in'] = False
+                return render(request, 'drugapp/home.html')
+                
+
+        return render(request, 'users/login.html', {'msg': 'Invalid credenials', 'form': UserLoginForm()})
+
+def login_db_manager(request):
+    if request.method == 'GET':
+
+        context = {'form': DBManagerLoginForm()}
+        return render(request, 'users/login.html', context)
+
+    elif request.method == 'POST':
+
+        form = DBManagerLoginForm(request.POST)
+        if form.is_valid():
+            f = form.cleaned_data
+            sha256password = hashlib.sha256(f['password'].encode('utf-8')).hexdigest()
+            if check_credentials_db_manager(f['username'], sha256password):
+                
+                request.session['database_manager_logged_in'] = True
+                request.session['user_logged_in'] = False
+                return render(request, 'drugapp/home.html')
+                
+
+        return render(request, 'users/login.html', {'msg': 'Invalid credenials', 'form': DBManagerLoginForm()})
         
+def logout(request):
+    if request.method == 'GET':
+        request.session['user_logged_in'] = False
+        request.session['database_manager_logged_in'] = False
+
+        return render(request, 'drugapp/home.html')
+
+def add_user(request):
+    if request.method == 'GET':
+
+        context = {'form': UserAddForm()}
+        return render(request, 'users/add_user.html', context)
+
+    elif request.method == 'POST':
+
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            f = form.cleaned_data
+            sha256password = hashlib.sha256(f['password'].encode('utf-8')).hexdigest()
+            msg = add_user_to_db(f['name'], f['institution'], sha256password)
+                
+        if msg:
+            return render(request, 'users/add_user.html', {'msg': msg, 'form': UserAddForm()})  
+
+        return render(request, 'users/add_user.html', {'msg': 'User created', 'form': UserAddForm()})
