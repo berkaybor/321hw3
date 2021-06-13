10c9dc7007a26784248db4451c5c076412050994
intern <elifnurakalin@gmail.com>
2021-06-13 12:13:09 +0300

    tiny fixes

--- drugapp/forms.py
+++ drugapp/forms.py
@@ -1,14 +1,18 @@
 from django import forms
 
+
 class ReactionAffinityEditForm(forms.Form):
     reaction_id = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Reaction ID'}))
     affinity = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Affinity'}))
 
+
 class GetDrugForm(forms.Form):
     drug_id = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'DrugBank ID'}))
 
+
 class GetProtein(forms.Form):
     uniprot_id = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'UniProt ID'}))
+
 
 class GetSideEffect(forms.Form):
     umls_cui = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'UMLS CUI'}))
